7d1a939d6b023d4c62d95ae52118f015256a4110
lethu <lethuzulu345@gmail.com>
2023-07-20 16:31:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import config from './config.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = React.useState([])
+    useEffect(() => {
+        axios
+            .get(`${config.api_base_url}/user`)
+            .then((response) => {
+                setUsers(response.data)
+                console.log('response   ', response)
+            })
+            .catch((error) => console.log(error))
+    }, [])
+
+    const [details, setDetails] = React.useState({
+        user_name: '',
+        password: '',
+    })
+
+    const handleChange = (event) => {
+        setDetails((details) => {
+            return {
+                ...details,
+                [event.target.name]: event.target.value,
+            }
+        })
+    }
+    const handleSubmit = async () => {
+        try {
+            const response = await axios.post(
+                `${config.api_base_url}/user/`,
+                details
+            )
+        } catch (error) {
+            console.log('error  ', error)
+        }
+    }
+
+    return (
+        <div className='App'>
+            <div>
+                <ul>
+                    {users.length > 0
+                        ? users.map((user, index) => {
+                              return (
+                                  <li key={index}>
+                                      {user.user_name} {' - '}{' '}
+                                      {user.password}
+                                  </li>
+                              )
+                          })
+                        : null}
+                </ul>
+            </div>
+
+            <div style={{ marginLeft: '10px' }}>
+                <input
+                    name='user_name'
+                    value={details.user_name}
+                    onChange={handleChange}
+                    placeholder='username'
+                />
+            </div>
+            <div style={{ marginLeft: '10px' }}>
+                <input
+                    name='user_password'
+                    value={details.password}
+                    onChange={handleChange}
+                    placeholder='password'
+                />
+            </div>
+            <button
+                style={{ marginLeft: '10px' }}
+                onClick={handleSubmit}>
+                submit
+            </button>
+        </div>
+    )
 }
 
-export default App;
+export default App
